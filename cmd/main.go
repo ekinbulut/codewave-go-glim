5b08ef91f0f9b23546ff64786a4348c37c84ba8c
ekinbulut <ekinbulut@gmail.com>
2022-08-07 21:47:37 +0300

    main: ratelimiter sample in main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"glim/internal"
+	"log"
+	"time"
+)
+
 func main() {
 	println("Hello, world!")
+
+	limiter := internal.NewRateLimiter(5, 5)
+
+	// call Allow() 3 times
+	for i := 0; i < 100; i++ {
+		// if Allow() return true, print "allow"
+		if limiter.Allow() {
+			log.Println("allow")
+		} else {
+			log.Println("not allow")
+			// sleep for 1 second
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	limiter.Stop()
+
 }
