99100225b64237bde5d69c7b6a3183ac60dcb55d
ekinbulut <ekinbulut@gmail.com>
2022-08-07 12:20:08 +0300

    pass: TestGetTokenFromBucket & RateLimiter created

--- test/ratelimiter_test.go
+++ test/ratelimiter_test.go
@@ -2,16 +2,35 @@ package test
 
 import (
 	"glim/internal"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetTokenFromBucket(t *testing.T) {
-	expected := &internal.Token{}
-	var tk internal.Token
+
 	rl := NewRateLimiter(10)
-	tk = rl.GetToken()
-	assert.True(t, reflect.DeepEqual(tk, expected))
+	for i := 0; i < 3; i++ {
+		rl.GetToken()
+	}
+	var size int = rl.GetBucketSize()
+	assert.Equal(t, 7, size)
+}
+
+type RateLimiter struct {
+	Bucket *internal.Bucket
+}
+
+func NewRateLimiter(bs int) *RateLimiter {
+	return &RateLimiter{
+		Bucket: internal.NewBucket(bs),
+	}
+}
+
+func (rl *RateLimiter) GetToken() {
+	rl.Bucket.RemoveOne()
+}
+
+func (rl *RateLimiter) GetBucketSize() (size int) {
+	return rl.Bucket.Size()
 }
